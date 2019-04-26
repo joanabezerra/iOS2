561da21ac7c71c9afcb75521e17f8a08d135cda9
Usuário Convidado <Guest@l2602micro28.local>
2019-04-26 19:45:47 -0300

    Initial Commit

---
+++ VideoGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGame
+//
+//  Created by Usuário Convidado on 26/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
